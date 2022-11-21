19f96e0952eb5314336556a5ee09f8d753bc0590
Teemu Spoof <tspoof@c2r4p11.hive.fi>
2022-11-21 17:08:10 +0200

    changed to use ft_strjoin

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,30 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 00:04:35 by tspoof            #+#    #+#             */
-/*   Updated: 2022/11/21 13:56:01 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/11/21 17:04:47 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static void	gnl_zerobuffer(char *buff)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < BUFFER_SIZE + 1)
+	{
+		buff[i] = '\0';
+		i++;
+	}
+}
 
 static size_t	gnl_newline_idx(char **buffer, int fd)
 {
 	size_t	i;
 	char	*new_line;
 	size_t	size;
+	char	sub_buff[BUFFER_SIZE + 1];
 
 	new_line = NULL;
 	while (!new_line)
@@ -27,16 +40,13 @@ static size_t	gnl_newline_idx(char **buffer, int fd)
 			i = (size_t)(new_line + 1 - *buffer);
 			return (i);
 		}
-		*buffer = ft_malloc_increase(*buffer, ft_strlen(*buffer) + BUFFER_SIZE + 1);
-		size = read(fd, *buffer + ft_strlen(*buffer), BUFFER_SIZE);
+		gnl_zerobuffer(sub_buff);
+		size = read(fd, sub_buff, BUFFER_SIZE);
 		if (!size)
-		{
-			if (ft_strlen(*buffer) > 0)
-				return (0);
 			break ;
-		}
+		*buffer = ft_strjoin(*buffer, sub_buff);
 	}
-	return (-1);
+	return (0);
 }
 
 char	*get_next_line(int fd)
@@ -44,22 +54,22 @@ char	*get_next_line(int fd)
 	static char	*buffer = NULL;
 	char		*tmp;
 	size_t		i;
-	char 		*line;
+	char		*line;
 
 	if (fd == -1)
 		return (NULL);
 	if (!buffer)
-	{
 		buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		buffer[0] = '\0';
-	}
 	if (!buffer)
 		return (NULL);
 	i = gnl_newline_idx(&buffer, fd);
-	if (i == (size_t)-1)
-		return (NULL);
 	if (i == 0)
-		return (buffer);
+	{
+		if (ft_strlen(buffer) > 0)
+			return (buffer);
+		else
+			return (NULL);
+	}
 	line = ft_substr(buffer, 0, i);
 	tmp = ft_substr(buffer, i, ft_strlen(buffer));
 	if (!tmp || !line)
